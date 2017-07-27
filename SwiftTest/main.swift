89b323464411da447793b9fd751ddb18f33a71ef
nieyubao <593647458@qq.com>
2017-07-27 11:22:26 +0800

    test

--- SwiftTest/main.swift
+++ SwiftTest/main.swift
@@ -78,6 +78,7 @@ let interestingNumbers = [
     "Prime": [2, 3, 5, 7, 11, 13],
     "Fibonacci": [1, 1, 2, 3, 5, 8],
     "Square": [1, 4, 9, 16, 25],
+    "Square2": [1, 4, 9, 16, 2523]
 ]
 var largest = 0
 for (kind, numbers) in interestingNumbers {
